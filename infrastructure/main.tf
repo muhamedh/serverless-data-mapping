07c8607fb3f50c5767da83386e4192983749d5d2
muhamedh <muhammed.hamzic@gmail.com>
2023-07-03 14:23:58 +0200

    version match

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,11 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
+      version = "5.0.0"
     }
   }
   required_version = ">= 1.1.0"
@@ -17,7 +13,7 @@ terraform {
 
 module "lambda_function" {
   source = "terraform-aws-modules/lambda/aws"
-
+  version = "5.0.0"
   function_name = "data-mapping"
   description   = "Data mapping Lambda"
   handler       = "handler.handler"
