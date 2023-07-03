6a733c3bf57223575c61391d6e116add158a1896
muhamedh <muhammed.hamzic@gmail.com>
2023-07-03 09:37:43 +0200

    terraform pipeline starter

---
+++ infrastructure/main.tf
@@ -0,0 +1,31 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+module "lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "data-mapping"
+  description   = "Data mapping Lambda"
+  handler       = "handler.handler"
+  runtime       = "node18"
+
+  source_path = "../functions/dataMapping/src"
+
+  tags = {
+    Name = "data-mapping"
+  }
+}
