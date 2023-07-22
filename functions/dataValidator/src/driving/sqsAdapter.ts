e2cae42a920816eb39e825cb72136d685d964b72
muhamedh <muhammed.hamzic@gmail.com>
2023-07-22 22:26:22 +0200

    console log out

--- functions/dataValidator/src/driving/sqsAdapter.ts
+++ functions/dataValidator/src/driving/sqsAdapter.ts
@@ -4,7 +4,10 @@ const sqsAdapter = async (event: SQSEvent) => {
     //TODO write a test to check if validateDocument function gets called x times if x sqs are in event.Records[x]
     
     for(const record of event.Records as SQSRecord[]){
-        console.log(record?.body);
+        console.log(record);
+        const r = JSON.parse(record?.body);
+
+        console.log(r?.s3?.object?.key);
     }
     return true;
 }
