66cc1e3922119c5b1fd84735c4746f013f88a0df
muhamedh <muhammed.hamzic@gmail.com>
2023-07-23 10:59:14 +0200

    console log event

--- functions/dataValidator/src/driving/sqsAdapter.ts
+++ functions/dataValidator/src/driving/sqsAdapter.ts
@@ -3,6 +3,7 @@ import { validateDocument } from "../domain/validateDocument";
 import { S3Record } from "../types/s3.type";
 const sqsAdapter = async (event: SQSRecord) => {
     //TODO write a test to check if validateDocument function gets called x times if x sqs are in event.Records[x]
+    console.log(event);
     const s3Records: S3Record[] = JSON.parse(event?.body).Records as S3Record[];
     await validateDocument(s3Records);
 }
