74d57a6542b836ea58fd2680c6e8d1c9a5da1a8a
muhamedh <muhammed.hamzic@gmail.com>
2023-07-23 14:44:54 +0200

    add debug logging

--- functions/dataValidator/src/driven/s3Adapter.ts
+++ functions/dataValidator/src/driven/s3Adapter.ts
@@ -1,13 +1,16 @@
 import { S3Client, S3ClientConfig, GetObjectCommand } from "@aws-sdk/client-s3";
 
 const readObject = async(bucketName: string, key: string) => {
+    console.log('bucketName: ' + bucketName + " key: " + key);
     const s3Client = new S3Client({});
     const input = {
         Bucket: bucketName,
         Key: key
     }
     const command = new GetObjectCommand(input);
-    return await s3Client.send(command);
+    const response = await s3Client.send(command);
+    console.log(JSON.stringify(response));
+    return response;
 }
 
 const s3Adapter = async (record:any) => { 
