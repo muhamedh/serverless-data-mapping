4bd095608a09535ee595947bce308816e58a5976
muhamedh <muhammed.hamzic@gmail.com>
2023-07-26 20:46:29 +0200

    add db auto scalling

--- infrastructure/product-db.tf
+++ infrastructure/product-db.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "product_db" {
   name           = "product_db"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 10
-  write_capacity = 10
+  read_capacity  = 5
+  write_capacity = 5
   stream_view_type = "NEW_IMAGE"
   stream_enabled   = true
 
@@ -18,5 +18,56 @@ resource "aws_dynamodb_table" "product_db" {
   attribute {
     name = "SKUNumber"
     type = "S"  # S represents String data type
+  }
+
+  lifecycle {
+    ignore_changes = [read_capacity, write_capacity]
+  }
+}
+resource "aws_appautoscaling_target" "product_table_read_target" {
+  max_capacity       = 25
+  min_capacity       = 5
+  resource_id        = "table/${aws_dynamodb_table.product_db.name}"
+  scalable_dimension = "dynamodb:table:ReadCapacityUnits"
+  service_namespace  = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "product_table_read_policy" {
+  name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.product_table_read_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.product_table_read_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.product_table_read_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.product_table_read_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBReadCapacityUtilization"
+    }
+
+    target_value = 70.0
+  }
+}
+
+resource "aws_appautoscaling_target" "product_table_write_target" {
+  max_capacity       = 25
+  min_capacity       = 5
+  resource_id        = "table/${aws_dynamodb_table.product_db.name}"
+  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
+  service_namespace  = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "product_table_write_policy" {
+  name               = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.product_table_write_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.product_table_write_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.product_table_write_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.product_table_write_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
+    }
+
+    target_value = 70.0
   }
 }
