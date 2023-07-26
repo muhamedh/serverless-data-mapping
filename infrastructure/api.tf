9cf22cde0a4360dd4eda8a3c52e3615628d028c3
muhamedh <muhammed.hamzic@gmail.com>
2023-07-26 08:59:18 +0200

    update needed policies

--- infrastructure/api.tf
+++ infrastructure/api.tf
@@ -1,7 +1,7 @@
 resource "aws_api_gateway_rest_api" "product_api" {
   name        = "Product Information API"
   description = "API exposing mapped product information"
-  body        = file("./openapi/openapi.yaml")
+  body        = template_file.aws_api_product_openapi_document
 }
 
 resource "aws_api_gateway_deployment" "product_api_deployment" {
@@ -13,4 +13,51 @@ resource "aws_api_gateway_stage" "product_api_stage" {
   stage_name    = "dev"
   rest_api_id   = aws_api_gateway_rest_api.product_api.id
   deployment_id = aws_api_gateway_deployment.product_api_deployment.id
+}
+
+data "template_file" "aws_api_product_openapi_document" {
+  template = file("./apispec/openapi.yaml")
+
+  vars = {
+    API_GATEWAY_ROLE_ARN = aws_iam_role.product_api_gw_role.arn
+    READ_TIMEOUT                 = "1000"
+    DYNAMO_PRODUCT_TABLE         = aws_dynamodb_table.product_db.id
+  }
+}
+
+resource "aws_iam_role" "product_api_gw_role" {
+  name               = "Product API Gateway Role"
+  assume_role_policy = jsonencode({
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Action: "sts:AssumeRole",
+        Effect: "Allow",
+        Principal: {
+          Service: "apigateway.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "product_api_gw_policy" {
+  name        = "Product API Gateway Policy"
+  description = "Policy giving access to dynamoDB read"
+
+  policy = jsonencode({
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Action: ["dynamodb:GetItem","dynamodb:Query","dynamodb:Scan"],
+        Effect: "Allow",
+        Resource: "${aws_dynamodb_table.product_db.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "api_gw_policy_attachment" {
+  policy_arn = aws_iam_policy.product_api_gw_policy.arn
+  role       = aws_iam_role.product_api_gw_role.name
 }
--- infrastructure/product-db.tf
+++ infrastructure/product-db.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "product_db" {
   name           = "product_db"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
+  read_capacity  = 10
+  write_capacity = 10
 
   # Define primary key
   hash_key       = "ProductID"
